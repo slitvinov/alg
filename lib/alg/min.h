d46f47b822b5c80f56a9cd63a36e64c016336197
Sergey Litvinov <slitvinov@gmail.com>
2018-06-11 16:49:40 +0200

    use only steppest des

--- lib/alg/min.h
+++ lib/alg/min.h
@@ -2,7 +2,8 @@
 
 typedef struct T T;
 
-enum {CONJUGATE_FR, CONJUGATE_PR, VECTOR_BFGS2, VECTOR_BFGS, STEEPEST_DESCENT};
+/* enum {CONJUGATE_FR, CONJUGATE_PR, VECTOR_BFGS2, VECTOR_BFGS, STEEPEST_DESCENT}; */
+enum {STEEPEST_DESCENT};
 
 typedef int (*AlgMinF )(int n, real *xx, real *yy, real *zz, void *param, /**/ real *E);
 typedef int (*AlgMinDF)(int n, real *xx, real *yy, real *zz, void *param, /**/ real *fx, real *fy, real *fz);
@@ -12,10 +13,10 @@ int alg_min_fin(T*);
 
 int alg_min_iterate(T*);
 
-int  alg_min_force   (T*, /**/ real **fx, real **fy, real **fz);
-int  alg_min_position(T*, /**/ real  **x, real  **y, real  **z);
+int  alg_min_force   (T*, /**/ real**, real**, real**);
+int  alg_min_position(T*, /**/ real**, real**, real**);
 real alg_min_energy(T*);
 
-int alg_min_test_gradient(T*);
+int alg_min_test_force(T*, real eps);
 
 #undef T
