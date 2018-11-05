a0b35a284a5f7e5a69f02a533ffde0db5582d82a
Sergey Litvinov <slitvinov@gmail.com>
2018-11-05 16:21:14 +0100

    add a test

--- example/eig/main.c
+++ example/eig/main.c
@@ -9,7 +9,13 @@
 #include <alg/eig.h>
 
 int main(void) {
-    real m[9];
-    real e0[3], e1[3], e2[3];
-    alg_eig_vectors(m, e0, e1, e2);
+    enum {X, Y, Z};
+    real m[6] = {1, 1/2.0, 1/3.0,
+                 1/3.0, 1/4.0,
+                 1/5.0};
+    real a[3], b[3], c[3];
+    alg_eig_vectors(m, a, b, c);
+    printf("%g %g %g\n", a[X], a[Y], a[Z]);
+    printf("%g %g %g\n", b[X], b[Y], b[Z]);
+    printf("%g %g %g\n", c[X], c[Y], c[Z]);
 }
--- lib/eig.c
+++ lib/eig.c
@@ -8,33 +8,37 @@
 
 #include "alg/eig.h"
 
-#define D (3)
-
 static real get(const gsl_matrix *m, int i, int j) { return gsl_matrix_get(m, i, j); }
 
-int alg_eig_vectors(const real *data, /**/ real e0[3], real e1[3], real e2[3]) {
+int alg_eig_vectors(const real A[6], /**/ real e0[3], real e1[3], real e2[3]) {
+    enum {XX, XY, XZ, YY, YZ, ZZ};
+    enum {YX = XY, ZX = XZ, ZY = YZ};
     enum {X, Y, Z};
-    double data_[D*D];
 
-    for (int i = 0; i < D*D; i++)
-        data_[i] = data[i];
+    double B[3*3];
+    gsl_matrix_view m;
+    gsl_vector *eval;
+    gsl_matrix *evec;
+    gsl_eigen_symmv_workspace *w;
+    int i;
 
-    gsl_matrix_view m
-        = gsl_matrix_view_array (data_, D, D);
-    gsl_vector *eval = gsl_vector_alloc (D);
-    gsl_matrix *evec = gsl_matrix_alloc (D, D);
-    gsl_eigen_symmv_workspace * w =
-        gsl_eigen_symmv_alloc (D);
+    eval = gsl_vector_alloc(3);
+    evec = gsl_matrix_alloc(3, 3);
+    w = gsl_eigen_symmv_alloc(3);
+
+    i = 0;
+    B[i++] = A[XX]; B[i++] = A[XY]; B[i++] = A[XZ];
+    B[i++] = A[YX]; B[i++] = A[YY]; B[i++] = A[YZ];
+    B[i++] = A[ZX]; B[i++] = A[ZY]; B[i++] = A[ZZ];
+    m = gsl_matrix_view_array(B, 3, 3);
     gsl_eigen_symmv (&m.matrix, eval, evec, w);
-    gsl_eigen_symmv_free (w);
-    gsl_eigen_symmv_sort (eval, evec,
-                          GSL_EIGEN_SORT_ABS_ASC);
-
+    gsl_eigen_symmv_sort(eval, evec, GSL_EIGEN_SORT_ABS_ASC);
     e0[X] = get(evec, X, X); e0[Y] = get(evec, Y, X); e0[Z] = get(evec, Z, X);
     e1[X] = get(evec, X, Y); e1[Y] = get(evec, Y, Y); e1[Z] = get(evec, Z, Y);
     e2[X] = get(evec, X, Z); e2[Y] = get(evec, Y, Z); e2[Z] = get(evec, Z, Z);
-    gsl_vector_free (eval);
-    gsl_matrix_free (evec);
 
+    gsl_vector_free(eval);
+    gsl_matrix_free(evec);
+    gsl_eigen_symmv_free(w);
     return HE_OK;
 }
