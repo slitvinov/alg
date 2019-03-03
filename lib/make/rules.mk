4504f9c62f9e9feb6ac3dedbcfcb17d08b8f2677
Sergey Litvinov <slitvinov@gmail.com>
2019-03-03 16:31:24 +0100

    alg uses co.

--- lib/make/rules.mk
+++ lib/make/rules.mk
@@ -1,14 +1,14 @@
 install: $L $H
-	he.install $L $(PREFIX)/lib
-	he.install $H $(PREFIX)/include
+	co.install $L $(PREFIX)/lib
+	co.install $H $(PREFIX)/include
 
 libalg_s.a: $O $(O_S); ar rv $@ $O $(O_S) && ranlib $@
 libalg_d.a: $O $(O_D); ar rv $@ $O $(O_D) && ranlib $@
 $O : $H $I
 
-%.o:   %.c; $(CC) $(CFLAGS) $(ALG_CFLAGS) $(HE_S_CFLAGS) -c -o $@ $<
-%.s.o: %.c; $(CC) $(CFLAGS) $(ALG_CFLAGS) $(HE_S_CFLAGS) -c -o $@ $<
-%.d.o: %.c; $(CC) $(CFLAGS) $(ALG_CFLAGS) $(HE_D_CFLAGS) -c -o $@ $<
+%.o:   %.c; $(CC) $(CFLAGS) $(ALG_CFLAGS) $(CO_S_CFLAGS) -c -o $@ $<
+%.s.o: %.c; $(CC) $(CFLAGS) $(ALG_CFLAGS) $(CO_S_CFLAGS) -c -o $@ $<
+%.d.o: %.c; $(CC) $(CFLAGS) $(ALG_CFLAGS) $(CO_D_CFLAGS) -c -o $@ $<
 
 clean:; rm -f $O $(O_S) $(O_D) $L
 test:
