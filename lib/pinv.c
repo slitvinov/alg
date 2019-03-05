eee6ed01a97b2c8e559174e2b2a82c1d5a16c3a7
Sergey Litvinov <slitvinov@gmail.com>
2019-03-05 09:00:12 +0100

    alloc/free

--- lib/pinv.c
+++ lib/pinv.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <gsl/gsl_linalg.h>
 
 #include <real.h>
+
 #include <co/err.h>
 #include <co/memory.h>
 
@@ -10,16 +12,40 @@
 
 typedef struct T T;
 struct T {
+    gsl_matrix *A, *V;
+    gsl_vector *S, *work;
 };
 
 int alg_pinv_ini(int dim, T **pq) {
     T *q;
     MALLOC(1, &q);
+
+    if ((q->A = gsl_matrix_alloc(dim, dim)) == NULL) {
+        MSG("allocation failed for q->A");
+        return 1;
+    }
+    if ((q->V = gsl_matrix_alloc(dim, dim)) == NULL) {
+        MSG("allocation failed for q->V");
+        return 1;
+    }
+    if ((q->S = gsl_vector_alloc(dim)) == NULL) {
+        MSG("allocation failed for q->S");
+        return 1;
+    }
+    if ((q->work = gsl_vector_alloc(dim)) == NULL) {
+        MSG("allocation failed for q->work");
+        return 1;
+    }
+    
     *pq = q;
     return CO_OK;
 }
 
 int alg_pinv_fin(T *q) {
+    gsl_matrix_free(q->A);
+    gsl_matrix_free(q->V);
+    gsl_vector_free(q->S);
+    gsl_vector_free(q->work);        
     FREE(q);
     return CO_OK;
 }
