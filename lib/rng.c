a41bf4e5587e0bc260694e1a1997cfb189e9dddc
Sergey Litvinov <slitvinov@gmail.com>
2019-03-28 22:08:36 +0100

    add alg_rng_gaussian and alg_rng_uniform

---
+++ lib/rng.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+#include <real.h>
+#include <co/err.h>
+#include <co/memory.h>
+
+#include "alg/rng.h"
+
+#define T AlgRng
+
+struct T
+{
+	gsl_rng * r;
+};
+
+int
+alg_rng_ini(T** pq)
+{
+	T *q;
+	gsl_rng * r;
+	const gsl_rng_type * R;
+
+	MALLOC(1, &q);
+	R = gsl_rng_default;
+	gsl_rng_env_setup();
+	r = gsl_rng_alloc(R);
+	q->r = r;
+	*pq = q;
+	return CO_OK;
+}
+
+int
+alg_rng_free(T *q)
+{
+	gsl_rng_free(q->r);
+	FREE(q);
+	return CO_OK;
+}
+
+real
+alg_rng_gaussian(T *q, real sigma)
+{
+	return gsl_ran_gaussian(q->r, sigma);
+}
+
+real
+alg_rng_uniform(T *q, real a, real b)
+{
+	return gsl_ran_flat(q->r, a, b);
+}
+
