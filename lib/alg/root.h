0a0d68c8aa82e8833dacc07eca8d0d375e678c51
Sergey Litvinov <slitvinov@gmail.com>
2019-04-25 11:31:39 +0200

    add an example for root

--- lib/alg/root.h
+++ lib/alg/root.h
@@ -5,5 +5,7 @@ enum {BISECTION, FALSEPOS, BRENT};
 int alg_root_ini(int, T**);
 int alg_root_fin(T*);
 int alg_root_apply(T*, real, real, real (*)(real, void*), void*, /**/ real*);
+const char* alg_root_name(T*);
+int alg_root_niter(T*);
 
 #undef T
--- lib/root.c
+++ lib/root.c
@@ -9,8 +9,8 @@
 
 #include "alg/root.h"
 
-static const double EPSABS = 0;
-static const double EPSREL = 1e-6;
+static const double EPSABS = 1e-10;
+static const double EPSREL = 1e-8;
 static const size_t NITER = 1000;
 
 #define T AlgRoot
@@ -39,9 +39,11 @@ G(double x, void *vp)
 
 struct T {
 	gsl_root_fsolver *s;
+	int niter;
 };
 
-int alg_root_ini(int type, T **pq)
+int
+alg_root_ini(int type, T **pq)
 {
 	T *q;
 	int i, n;
@@ -50,30 +52,30 @@ int alg_root_ini(int type, T **pq)
 
 	FILL;
 	MALLOC(1, &q);
-	
-	if (s == NULL)
-		ERR(CO_MEMORY, "fail to allocate root");
 	n = sizeof(Type)/sizeof(Type[0]);
-	i = 0;
-	for (i = 0; i++; /**/ )
+	for (i = 0; /**/; i++)
 		if (i == n)
 			ERR(CO_INDEX, "unknown type: %d", type);
 		else if (type == Type[i])
 			break;
 	s = gsl_root_fsolver_alloc(Stype[i]);	
+	if (s == NULL)
+		ERR(CO_MEMORY, "fail to allocate root");
 	q->s = s;
 	*pq = q;
 	return CO_OK;
 }
 
-int alg_root_fin(T *q)
+int
+alg_root_fin(T *q)
 {
 	gsl_root_fsolver_free(q->s);
 	FREE(q);
 	return CO_OK;
 }
 
-int alg_root_apply(T *q, real lo, real hi, real (*f)(real, void*), void *p, real *r)
+int
+alg_root_apply(T *q, real lo, real hi, real (*f)(real, void*), void *p, real *r)
 {
 	gsl_function F;
 	Param param;
@@ -92,13 +94,29 @@ int alg_root_apply(T *q, real lo, real hi, real (*f)(real, void*), void *p, real
 
 	do {
 		i++;
-		status = gsl_root_fsolver_iterate (s);
+		status = gsl_root_fsolver_iterate(s);
+		if (status != GSL_SUCCESS)
+			ERR(CO_NUM, "staus: %s", gsl_strerror(status));
 		*r = gsl_root_fsolver_root(s);
       		lo = gsl_root_fsolver_x_lower(s);
       		hi = gsl_root_fsolver_x_upper(s);
 		status = gsl_root_test_interval(lo, hi, EPSABS, EPSREL);
 		if (status == GSL_SUCCESS)
 			break;
-	} while (status == GSL_CONTINUE && i < NITER);                                  
+	} while (status == GSL_CONTINUE && i < NITER);
+	q->niter = i;
+                             
 	return CO_OK;
+}
+
+const char*
+alg_root_name(T *q)
+{
+	return gsl_root_fsolver_name(q->s);
+}
+
+int
+alg_root_niter(T *q)
+{
+	return q->niter;
 }
