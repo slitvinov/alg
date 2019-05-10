be4014ed440a797cc5e12d8376a14adf83e12508
Sergey Litvinov <slitvinov@gmail.com>
2019-05-10 17:22:20 +0200

    wip ode

--- example/ode/main.c
+++ example/ode/main.c
@@ -8,10 +8,84 @@
 
 #include <real.h>
 #include <co/err.h>
+#include <co/memory.h>
 #include <co/macro.h>
 
+static const real EPSABS =  1e-8;
+static const real EPSREL = 1e-8;
+#define T Ode
+typedef struct Ode Ode;
+enum
+{
+	RK2, RK4, RKF45, RKCK, RK8PD
+};
+int ode_ini(int type, int dim, real dt, int (*)(real, const real*, real *dy, void*), void*, T**);
+int ode_fin(T*);
+int ode_apply(T*, real *time, real dt, real*);
+#undef T
+
+#define T Ode
+struct T
+{
+	gsl_odeiv2_driver *d;
+	void *param;
+};
+
+typedef struct Param Param;
+struct Param
+{
+	void *param;
+	double *y, *dy;
+	int dim;
+};
+
+static int Type[] =
+{
+	RK2, RK4, RKF45, RKCK, RK8PD
+};
+#define FILL \
+	do { \
+		i = 0; \
+		Stype[i++] = gsl_odeiv2_step_rk2; \
+		Stype[i++] = gsl_odeiv2_step_rk4; \
+		Stype[i++] = gsl_odeiv2_step_rkf45; \
+		Stype[i++] = gsl_odeiv2_step_rk8pd; \
+	} while(0);
+
 int
-func (double t, const double y[], double f[],
+ode_ini(int type, int dim, real dt, int (*f)(real, const real*, real *dy, void*), void *param, T **pq)
+{	
+	T *q;
+	int i, n;
+	gsl_odeiv2_driver *d;
+	gsl_odeiv2_system sys;
+	const gsl_odeiv2_step_type *Stype[99];
+	
+	FILL;
+	MALLOC(1, &q);
+	n = sizeof(Type)/sizeof(Type[0]);
+	i = 0;
+	while (1) {
+		if (i == n)
+			ERR(CO_INDEX, "unknown type %d", type);
+		if (Type[i] == type)
+			break;
+		i++;
+	}
+	sys.function  = NULL;
+	sys.jacobian = NULL;
+	sys.dimension = dim;
+	sys.params = NULL;	
+	d = gsl_odeiv2_driver_alloc_y_new(&sys, Stype[i], dt, EPSABS, EPSREL);
+	if (d == NULL)
+		ERR(CO_MEMORY, "fail to allocate ode driver");	    	 
+	q->d = d;
+	*pq = q;
+	return CO_OK;
+}
+
+int
+func (__UNUSED double t, const double y[], double f[],
 void *params)
 {
 	double mu = *(double *)params;
@@ -25,7 +99,8 @@ main (void)
 {
 	double mu = 10;
 	gsl_odeiv2_system sys = {
-		func, NULL, 2, &mu	};
+		func, NULL, 2, &mu
+	};
 
 	gsl_odeiv2_driver * d =
 	    gsl_odeiv2_driver_alloc_y_new (&sys, gsl_odeiv2_step_rk4,
