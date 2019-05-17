9fb6549978c30d080722ba70eb5c06d8216475f5
Sergey Litvinov <slitvinov@gmail.com>
2019-05-17 17:24:45 +0200

    add lib/linsolve.c

--- lib/make/hdr.mk
+++ lib/make/hdr.mk
@@ -1,3 +1,4 @@
 H = \
-alg/eig.h alg/integration.h alg/min.h alg/ode.h alg/pinv.h \
-alg/rng.h alg/root.h alg/special.h alg/spline.h alg/x.h
+alg/eig.h alg/integration.h alg/linsolve.h alg/min.h alg/ode.h \
+alg/pinv.h alg/rng.h alg/root.h alg/special.h alg/spline.h \
+alg/x.h
