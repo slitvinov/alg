b8d5ec2bcf33cfdae321d6bed0a854ddb082fc62
Sergey Litvinov <slitvinov@gmail.com>
2020-02-15 18:37:52 +0100

    add linsolve_sym

--- lib/alg/linsolve.h
+++ lib/alg/linsolve.h
@@ -5,6 +5,7 @@ int lin_solve_ini(int n, T **);
 
 /* Ax = b */
 int lin_solve_apply(T *, const real * A, const real * b, real * x);
+int lin_solve_sym(T *, const real * A, const real * b, real * x);
 int lin_solve_fin(T *);
 
 #undef T
